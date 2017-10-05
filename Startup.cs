81a35e87ef80260194f1f949c41ee7bcd5e1f1a4
RaviMersat <32481322+RaviMersat@users.noreply.github.com>
2017-10-05 13:02:44 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MersatPOSApplication.Web.Startup))]
+namespace MersatPOSApplication.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
